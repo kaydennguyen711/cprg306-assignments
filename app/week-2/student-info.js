6f4e22977361626f028757323eb46880820f315d
SAITkaydennguyen711 <153153375+kaydennguyen711@users.noreply.github.com>
2024-09-24 17:13:15 -0600

    Update student-info.js

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -12,14 +12,3 @@ export default function StudentInfo() {
     </div>
   );
 }
-
-import StudentInfo from './student-info';
-
-export default function Page() {
-  return (
-    <main>
-      <h1>Shopping List</h1>
-      <StudentInfo />
-    </main>
-  );
-}
